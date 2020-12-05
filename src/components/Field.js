1eee3a691871403282df17a9f22845866ab0e7b1
Erik Nelson <erik.cse@gmail.com>
2020-12-04 16:45:00 -0800

    Moving field data to json file, multiple fields

--- src/components/Field.js
+++ src/components/Field.js
@@ -6,25 +6,20 @@ function getLine(coords){
     var a = {latitude: coords[0][0], longitude: coords[0][1]}
     var b = {latitude: coords[1][0], longitude: coords[1][1]}
 
-    var bearing = geolib.getRhumbLineBearing(a, b)
+    var bearing = geolib.getRhumbLineBearing(a, b) - 90
     //bearing = geolib.getGreatCircleBearing(a, b)
-    var dest = geolib.computeDestinationPoint(b, 4000000, bearing - 90)
+    var dest = geolib.computeDestinationPoint(b, 4000000, bearing)
     line[2] = [dest.latitude, dest.longitude]
-    dest = geolib.computeDestinationPoint(a, 4000000, bearing - 90)
+    dest = geolib.computeDestinationPoint(a, 4000000, bearing)
     line[3] = [dest.latitude, dest.longitude]
     line[4] = coords[0]
     return line
 }
 
-function Field() {
-    //Husky stadium field coordinates, go dawgs
-    var coords = [[47.650650, -122.302096],
-        [47.650235, -122.302297]]
-        //[47.649982, -122.301137]]
-        //[47.650398, -122.300938]]
-    var line = getLine(coords)
+function Field(params) {
+    var line = getLine(params.data.coords)
     return (
-        <GeodesicLine color="purple" positions={line} />
+        <GeodesicLine color={params.data.color} positions={line} />
     )
 }
 export default Field
@@ -32,8 +27,3 @@ export default Field
 // Data should be able to be two coordinates (endzone), plus name, plus color.  Maybe ID
 // Could switch to coordinate center (easily available on wiki and whatnot) + bearing.
 
-// Original coordinates for the stadium
-// [47.650650, -122.302096]
-// [47.650235, -122.302297]
-// [47.649982, -122.301137]
-// [47.650398, -122.300938]
--- src/components/MapWidget.js
+++ src/components/MapWidget.js
@@ -1,13 +1,17 @@
 import { MapContainer, TileLayer} from 'react-leaflet';
 import Field from './Field'
+import fieldData from '../fields.json' 
 function MapWidget() {
+    console.log(fieldData)
     return (
         <MapContainer id="mapid" center={[39.828, -98.579]} zoom={5}>
             <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Field />
+            {fieldData.map((field) =>
+                <Field id={field.name} data={field}/>
+            )}
         </MapContainer>
     )
 }
